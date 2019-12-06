becbb7e2c74ab6f6adc66fdd78a267555c91775f
Selt0 <chewedgum12@gmail.com>
2019-12-06 11:46:50 -0600

    Add README file

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-//reverse a string
-function reverse(string) {
-  //simple reverse string
-  //return string.split("").reverse().join("");
-
-  //to include emojis or other crazy characters
-  return Array.from(string).reverse().join("");
-}
-
-//function to detect palindrome, not case sensitive
-function palindrome(string) {
-  let original = string.toLowerCase();
-  return original === reverse(original);
-}
-
-//separates user email and domain
-function emailParts(string) {
-  //create an array including the characters
-//  let email = Array.from(string);
-//   //loop through array until '@' is found.
-//   for (let i = 0; i < email.length; i++) {
-//     if (email[i] === '@') {
-//       //slice starting from '@' location.
-//      let domain =  email.slice(i + 1).join("");
-//      let username = email.slice(0, i).join("");
-//      console.log(`Your user name is ${username}. The domain for the email address is ${domain}.`)
-//     }
-//   }
-  //print out  username and domain.
-  let email = string.toLowerCase().split("@");
-  console.log(`your username is ${email[0]} and the domain is ${email[1]}.`);
-  return email;
-}
-
+module.exports = Phrase;
 //extending built-in prototypes
 
 //reverse method added to String.prototype. Reverses the string
